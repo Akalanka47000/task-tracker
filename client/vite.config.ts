7e20a4fc7c2997aeaa2a55448eb265696a13e86d
Akalanka Perera <akalankaperera128@gmail.com>
2025-03-30 22:07:30 +0530

    ci: added vite allowed hosts variable

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   preview: {
     host: true,
-    port: 5173
+    port: 5173,
+    allowedHosts: process.env.VITE_ALLOWED_HOSTS?.split(',')
   },
   test: {
     environment: 'happy-dom'
